81f73b2266dbd9a9d40e36b16b2f112bb8a8e9f5
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-08-25 14:02:51 +0100

    Changed to 14.3 iOS for all targets.

---
+++ Cats/Model/Breed.swift
@@ -0,0 +1,47 @@
+//
+//  Breed.swift
+//  Cats
+//
+//  Created by Charlie Nguyen on 25/08/2021.
+//
+
+import Foundation
+
+struct Breed: Codable, CustomStringConvertible {
+    let id: String
+    let name: String
+    let temperament: String
+    let breedExplaination: String
+    let energyLevel: Int
+    let isHairless: Bool
+    let image: BreedImage
+    
+    var description: String {
+        return "Breed with name \(name) and id \(id), energy level: \(energyLevel), isHairless: \(isHairless ? "Yes" : "No")"
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case temperament
+        case breedExplaination = "description"
+        case energyLevel = "energy_Level"
+        case isHairless = "hairless"
+        case image
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try values.decode(String.self, forKey: .id)
+        name = try values.decode(String.self, forKey: .name)
+        temperament = try values.decode(String.self, forKey: .temperament)
+        breedExplaination = try values.decode(String.self, forKey: .breedExplaination)
+        energyLevel = try values.decode(Int.self, forKey: .energyLevel)
+        
+        let hairless = try values.decode(Int.self, forKey: .isHairless)
+        isHairless = hairless == 1
+        
+        image = try values.decode(BreedImage.self, forKey: .image)
+    }
+}
---
+++ Cats/Model/BreedImage.swift
@@ -0,0 +1,15 @@
+//
+//  BreedImage.swift
+//  Cats
+//
+//  Created by Charlie Nguyen on 25/08/2021.
+//
+
+import Foundation
+
+struct BreedImage: Codable {
+    let height: Int
+    let id: String
+    let url: String
+    let width: Int
+}
