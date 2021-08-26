00efb3d43fd207aa01793ca0e06bc55a486fde7c
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-08-26 12:14:38 +0100

    DetailView for selected cat implemented.

--- Cats/View/BreedDetailView.swift
+++ Cats/View/BreedDetailView.swift
@@ -10,9 +10,55 @@ import SwiftUI
 struct BreedDetailView: View {
     
     let breed: Breed
+    let imageSize: CGFloat = 300
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                if breed.image?.url != nil {
+                    AsyncImage(url: URL(string: breed.image!.url!)) { phase in
+                        if let image = phase.image {
+                            image.resizable()
+                                .scaledToFit()
+                                .frame(height: imageSize)
+                                .clipped()
+                        } else if phase.error != nil {
+                            Color.red // Indicates an error.
+                                .frame(width: imageSize, height: imageSize)
+                        } else {
+                            ProgressView()
+                                .frame(width: imageSize, height: imageSize)
+                        }
+                    }
+                } else {
+                    Color.gray.frame(height: imageSize)
+                }
+                
+                VStack(alignment: .leading, spacing: 15) {
+                    
+                    Text(breed.name)
+                        .font(.headline)
+                    Text(breed.temperament)
+                        .font(.footnote)
+                    Text(breed.breedExplaination)
+                    if breed.isHairless {
+                        Text("Hairless")
+                    }
+                    
+                    HStack {
+                        Text("Energy Level:")
+                        ForEach(1..<6) { id in
+                            Image(systemName: "star.fill")
+                                .foregroundColor(breed.energyLevel > id ? Color.accentColor : Color.gray)
+                        }
+                    }
+                    
+                    Spacer()
+                }
+                .padding()
+                .navigationBarTitleDisplayMode(.inline)
+            }
+        }
     }
 }
 
